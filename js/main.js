716ae0386eb752295fde077f826c4952631c71ea
oys-dev <fhfh8282@naver.com>
2022-08-30 12:00:10 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchInputEl = document.querySelector('.search input');
-
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
 const badgeEl = document.querySelector('header .badges');
 
 window.addEventListener('scroll', _.throttle(function (){
